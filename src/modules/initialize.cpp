106789a047918d504c267c2ef3b394ac9be64e6d
dealer-platform <florian.menges@wiwi.uni-regensburg.de>
2020-01-05 14:37:22 +0100

    inits removed
    inits removed

--- src/modules/initialize.cpp
+++ src/modules/initialize.cpp
@@ -12,19 +12,17 @@ ACTION reporting::init() {
     reguser("bob"_n, "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEArB488qKqhnQUlufw8xde\nnJHIyn0hXKNnjAj8l5N9a9+vK2EH1aAGT2aRCEJz2JmaLoCy2C1YoXvWlktISHKB\nTwnHhOInExLbDaU/WG0CHTU6l2rYzsb8kVAXsxz/zxMvXJjS9jELvtQfmT87/pMD\n0D94m14y6/2zMYCOr5a5wJmNZc4JtQQSWgi++hpuKqfI\n1TPCJLEmFs/lGzW43NFi\n0TpebY56uIpuGUikJ1gc1nX2uiFJG1dCA5mbCglj/RBRyrrPWomZNEty050LxDOq\nAliDqrCEoR8lbFv5f+Q/EJsHZD7P7dyZhJkQxK56W7927OdFLDWMWplbXif5RRNE\nHie0gEY425hdxveqPhQ18gLofvAI25CfPQbVPj8U4XgCoSCu4l2f2M0Xts9Z8yf5\ngGvczhY3lnzllreM557xGnXFwrIKaNPSygJbUd0KqN14kP/ySqES2C\nLacqRSSZSk\nRkvVPcqJ7V+4Lwz12PXb37CH5m5OFG77tA9bG55nLkBSzvuiLaE+r0F4LMfo5GGN\nQmuIawcHdhf05C2ZzlLkMmmq+OmEKpKfrhmfPJEe9emjKpXgT79xyUMQS5giBZKp\nKJ4z24clje6SciwMZIEoY0y+vPYe1vzhICjZ3NDi+hAWgrF+L7TJf86gS6c6u2u2\nO+PPjZWCDNv2g15fSFWID7kCAwEAAQ==",true,"QmSiogXzJpGenLurt3ofh33SBfPxc4sgwjoTteaTrVeGEs");
 	reguser("simon"_n, "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA25CA8aDWxPhNHCoKFXvG\n97kljaDR/weEYVFdEiFlEgz8plEgcEoz73yRKa3ZbAu1dzVeh1YXBZAmG6vxrv+S\n6elSUUbCrUGEAhzV+099xTKqy+hRV0PpC+nG6lImRMlRqSwdKBqWIIoyDmGx80B3\nutZ9/xVIRDziImI1zhCApaWxNVzkB73xeBR8VNRk8cAG\nuE5wlYHL4uk21w9JtylA\nh9jIMVDPflWhuWzieirzOR01VFbLQWO97NmgKkx5PxgyDXosRqc2llms5kqfMPfH\nKN0byCA52d0A1XB7wnrhmnn/t3fTT2Wo1ivYvj/TxD1o7q3VFQRNDWD+ah8lA/co\n0gJIqP+xs9fnE5oPeRNBqAfYBACE3AjjXBGyyD+tupyhqsA7xf+e1sPEtl1N2ZC4\nOjZW9kaRFIY+cVpo4apr8Ea6hTX1It7Ai2x29SIQvLkwCM+rDb7u2s\nIV2Q7002x0\nwV/JMfdrssi1y82ZTY7zPLm+kMMgfMY45GuFOPyLm4h6iNlKva0y5rkSieLwQwjc\ns5awzwL0+1sgveOW8mQmCPVEk6S3y39TUuVBZGHbcC6J4iYxZ5IFwrhGCh6+aBrE\nRdquwRQiHhTmsZMAH9Ab93j8t6/1d1MhbeVxYeO5uO5se5H/WkOtEnOomNHJnb+E\nDMp15vaMDmYxwT3aJCvnBEkCAwEAAQ==",true,"QmeybGaHdwrt2TyML57Gsm6dwktT5WpGFpns1G21LoXBZt");
 	
-	insertitem("peter"_n, "8867b0f58bab0b0bc37a8ddf50f7d822f91614c8950abbc79e2b458ebb936889", 25, "Threat Actor Leveraging Attack Patterns and Malware", "Threat Actor and Attack Pattern", true, true);
-	insertitem("peter"_n, "e43c66c297dc4d3f680dfdaaa6b9380a57d7f25cdd1eda740e19e460e723cab6", 16, "A malicious URL was seen on the network and an indicator was generated to capture this information.", "Sighting of an Indicator", true, true);
-	insertitem("emma"_n, "2db44de5b12085cfc9a7a7cb5f3090fbae62ae2c728e2113f30834dee5ca4744", 12, "This scenario consists of an indicator for an URL which is known to be malicious", "	Indicator for Malicious URL", true, true);
-	insertitem("emma"_n, "01f3d8da8677434c96398d4bfea0761f4dc4c791599035fb6b42ab6b7ebc0c2e", 12, "A Description of a simple indicator that represents a pattern for a file", "Malware Indicator for File Hash", true, true);
-	insertitem("peter"_n, "5b5d716bbd9abd3d10ee0938cd0a104e37b8cec69e97e597f246fb4a08a0a8c3", 14, "Yet another attack pattern", "Latest Attack Pattern", true, true);
-	insertitem("emma"_n, "05bad41c57932c044a7002a9338e3b34b05009907e3f171f89b4fcb2b9e3c887", 17, "Recognized intrusion set", "Intrusion set", true, true);
-
-
+	//insertitem("peter"_n, "8867b0f58bab0b0bc37a8ddf50f7d822f91614c8950abbc79e2b458ebb936889", 25, "Threat Actor Leveraging Attack Patterns and Malware", "Threat Actor and Attack Pattern", true, true);
+	//insertitem("peter"_n, "e43c66c297dc4d3f680dfdaaa6b9380a57d7f25cdd1eda740e19e460e723cab6", 16, "A malicious URL was seen on the network and an indicator was generated to capture this information.", "Sighting of an Indicator", true, true);
+	//insertitem("emma"_n, "2db44de5b12085cfc9a7a7cb5f3090fbae62ae2c728e2113f30834dee5ca4744", 12, "This scenario consists of an indicator for an URL which is known to be malicious", "	Indicator for Malicious URL", true, true);
+	//insertitem("emma"_n, "01f3d8da8677434c96398d4bfea0761f4dc4c791599035fb6b42ab6b7ebc0c2e", 12, "A Description of a simple indicator that represents a pattern for a file", "Malware Indicator for File Hash", true, true);
+	//insertitem("peter"_n, "5b5d716bbd9abd3d10ee0938cd0a104e37b8cec69e97e597f246fb4a08a0a8c3", 14, "Yet another attack pattern", "Latest Attack Pattern", true, true);
+	//insertitem("emma"_n, "05bad41c57932c044a7002a9338e3b34b05009907e3f171f89b4fcb2b9e3c887", 17, "Recognized intrusion set", "Intrusion set", true, true);
 
 	reporting::warning("reporting"_n,"Emotet continues to be among the most costly and destructive malware affecting governments. Its worm-like features result in rapidly spreading network-wide infections");
 
-	insertorder("peter"_n, 3, true);
-	insertorder("emma"_n, 0, false);
+	//insertorder("peter"_n, 3, true);
+	//insertorder("emma"_n, 0, false);
 
 	}
 }
